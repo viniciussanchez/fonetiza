c0f4be0bb1fe6f380d61da02de5f61189ee0bf48
viniciussanchez <viniciuss.sanchez@gmail.com>
2020-06-18 18:42:37 -0300

    Remover valores repetidos

--- src/Fonetiza.Impl.pas
+++ src/Fonetiza.Impl.pas
@@ -7,8 +7,10 @@ uses Fonetiza.Intf;
 type
   TFonetiza = class(TInterfacedObject, IFonetiza)
   private
+    function GerarConteudoFonetico(const AValue: string): string;
     function SomarCaracteres(const AValue: string): string;
     function SubstituirConteudos(const AValue: string; const AConteudo: TArray<TArray<string>>): string;
+    function RemoverCaracteresDuplicados(const AValue: string): string;
     function RemoverCaracteresEspeciais(const AValue: string): string;
     function RemoverConteudos(const AValue: string; const AConteudo: TArray<string>): string;
     function RemoverAcentuacoes(const AValue: string): string;
@@ -34,7 +36,8 @@ begin
   Result := Self.SubstituirConteudos(Result, LETRAS);
   Result := Self.SubstituirConteudos(Result, NUMEROS);
   Result := Self.SomarCaracteres(Result);
-//  Result := PhonetizerBR.phonetize(Result);
+  Result := Self.RemoverCaracteresDuplicados(Result);
+//  Result := Self.GerarConteudoFonetico(Result);
   Result := Self.SubstituirConteudos(Result, NOMES);
   Result := Self.SubstituirConteudos(Result, SINONIMOS);
 end;
@@ -58,6 +61,21 @@ type
   USAscii20127 = type AnsiString(20127);
 begin
   Result := string(USAscii20127(AValue));
+end;
+
+function TFonetiza.RemoverCaracteresDuplicados(const AValue: string): string;
+var
+  I: Integer;
+  LChar: Char;
+begin
+  LChar := ' ';
+  for I := 0 to Pred(AValue.Length) do
+  begin
+    if ((AValue[i] <> LChar) or (AValue[I] = ' ') or ((AValue[i] >= '0') and (AValue[I] <= '9')) or ((AValue[I] = 'S') and (AValue[I - 1] = 'S') and ((I > 1) and (AValue[I - 2] <> 'S')))) then
+      Result := Result + AValue[I];
+    LChar := AValue[I];
+  end;
+  Result := Result.Trim;
 end;
 
 function TFonetiza.RemoverCaracteresEspeciais(const AValue: string): string;
@@ -87,46 +105,40 @@ end;
 
 function TFonetiza.SomarCaracteres(const AValue: string): string;
 var
-  LPalavra: string;
-  LPalavras: TArray<string>;
   LSoma, LValor: Integer;
+  LPalavras: TArray<string>;
+  I: Integer;
 begin
-  LSoma := 0;
-	LPalavras := AValue.Split([' ']);
-  for LPalavra in LPalavras do
+  LPalavras := AValue.Split([' ']);
+  for I := 0 to Pred(Length(LPalavras)) do
   begin
-    if LPalavra.Trim.Equals('E') then
+    if LPalavras[I].Equals('E') then
       Continue;
-    if LPalavra.Trim.Equals('MIL') then
+    if LPalavras[I].Equals('MIL') then
     begin
-      if LSoma = 0 then
-        LSoma := 1000
-      else
+      if LSoma <> 0 then
       begin
         LSoma := LSoma * 1000;
         Continue;
       end;
-    end
-    else
-    begin
-      LValor := StrToIntDef(LPalavra, 0);
-      if LValor <> 0 then
-      begin
-        LSoma := LSoma + LValor;
-        Continue;
-      end
-      else
-      begin
-        if LSoma <> 0 then
-          Result := Result + LSoma.ToString;
-        LSoma = 0;
-        Continue;
-      end;
+      LSoma := 1000;
+      Result := Result + LPalavras[I];
+      Continue;
     end;
-    Result := Result + LPalavra;
+    LValor := StrToIntDef(LPalavras[I], 0);
+    if LValor <> 0 then
+    begin
+      if LSoma = 0 then
+        Result := Result + LPalavras[I];
+      LSoma := LSoma + LValor;
+      Continue;
+    end;
+    if LSoma <> 0 then
+      Result := Result + LSoma.ToString;
+    LSoma := 0;
   end;
-	if LSoma <> 0 then
-  	Result := Result + LSoma.ToString;
+  if LSoma <> 0 then
+    Result := Result + LSoma.ToString;
 end;
 
 function TFonetiza.SubstituirConteudos(const AValue: string; const AConteudo: TArray<TArray<string>>): string;
@@ -150,6 +162,11 @@ begin
       Result := Result + ' ';
     Result := Result + LResultado;
   end;
+end;
+
+function TFonetiza.GerarConteudoFonetico(const AValue: string): string;
+begin
+
 end;
 
 end.
